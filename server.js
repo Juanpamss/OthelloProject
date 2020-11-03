401462990c2ccec4bdb1b87d53286453536da117
Juanpamss <juanpa_mss@hotmail.com>
2020-11-02 23:47:26 -0500

    Set up web server and index page

---
+++ server.js
@@ -0,0 +1,29 @@
+/* Including static file webserver libraries*/
+var static = require('node-static');
+
+/*Include HTTP server library*/
+var http = require('http');
+
+/*Assume this is running on a web server (Heroku)*/
+var port = process.env.PORT;
+var directory = __dirname + '/public';
+
+/*If this is not a web server, the readjust the port to localhost*/
+if(typeof port == "undefined" || port){
+    directory = './public';
+    port = 8080;
+}
+
+/*Set up static web server*/
+var file = new static.Server(directory);
+
+/*Construct http server to get file from the file server*/
+var app = http.createServer(
+    function (request,response) {
+        request.addListener('end', function () {
+            file.serve(request,response);
+        }).resume();
+    }
+).listen(port);
+
+console.log('Server running at: ' + port);
