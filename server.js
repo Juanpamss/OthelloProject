89b6013402eacf001ebd85240aa76cd2b702a30d
Juanpamss <juanpa_mss@hotmail.com>
2020-11-20 17:34:09 -0500

    Login, Register, Join Rooms
    Login and Register fixed and changed the way they look.
    Join room functionality is working with multiple players

--- server.js
+++ server.js
@@ -128,10 +128,14 @@ let server = https.createServer(httpsOptions,app).listen(port);
 console.log('Server running at: ' + port);
 
 /*****Configure socket server*****/
+var players = [];
+
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket){
     /*Log activity*/
+    log('Client connection by: ' + socket.id)
+
     function log(){
         var array = ['Server log message'];
         for (var i = 0; i < arguments.length; i++){
@@ -145,14 +149,28 @@ io.sockets.on('connection', function (socket){
 
     log('Client connected to the server');
 
-    socket.on('disconnect', function(socket){
-        log('Client disconnected from the server');
+    socket.on('disconnect', function(){
+        log('Client disconnected ' + JSON.stringify(players[socket.id]));
+
+        if('undefined' !== typeof players[socket.id] && players[socket.id]){
+
+            const username = players[socket.id].username;
+            const room = players[socket.id].room;
+            const payload = {
+                username: username,
+                socket_id: socket.id
+            }
+
+            delete players[socket.id]
+            io.in(room).emit('player_disconnected', payload)
+        }
+
     });
 
     /*Join room command*/
     /**/
     socket.on('join_room', function(payload){
-        log('server received command', 'join_room', payload)
+        log('\'join_room command\' command' +JSON.stringify(payload))
         if(('undefined' === typeof payload) || !payload){
             var error_message = 'join_room had no payload'
             log(error_message)
@@ -164,6 +182,7 @@ io.sockets.on('connection', function (socket){
             return;
         }
 
+        /*Check that the payload has a room to join*/
         var room = payload.room
         if(('undefined' === typeof room) || !room){
             var error_message = 'join_room did not specified a room'
@@ -176,6 +195,7 @@ io.sockets.on('connection', function (socket){
             return;
         }
 
+        /*Check that a user has been provided*/
         var username = payload.username
         if(('undefined' === typeof username) || !username){
             var error_message = 'join_room did not specified a username'
@@ -188,30 +208,43 @@ io.sockets.on('connection', function (socket){
             return;
         }
 
+        /*Store info about new player*/
+
+        players[socket.id] = {}
+        players[socket.id].username = username;
+        players[socket.id].room = room;
+
+        /*Allow user to join the room*/
         socket.join(room);
 
         var roomObject = io.sockets.adapter.rooms[room]
-        if(('undefined' === typeof roomObject) || !roomObject){
-            var error_message = 'join_room could not create a room'
-            log(error_message)
-            socket.emit('join_room_response',{
-                result: 'fail',
-                message: error_message
-            });
 
-            return;
-        }
+        /*Announce that a player joined*/
 
         var numClients = roomObject.length
         var success_data = {
             result: 'success',
             room: roomObject,
             username: username,
-            membership: (numClients + 1)
+            socket_id: socket.id,
+            membership: numClients
         }
 
         io.sockets.in(room).emit('join_room_response', success_data)
-        log('Room ' + room + ' was joined by ' + username)
+
+        for(var socket_in_room in roomObject.sockets){
+            var success_data = {
+                result: 'success',
+                room: room,
+                username: players[socket_in_room].username,
+                socket_id: socket_in_room,
+                membership: numClients
+            }
+
+            socket.emit('join_room_response', success_data)
+        }
+
+        log('join_room_success')
     });
 });
 
