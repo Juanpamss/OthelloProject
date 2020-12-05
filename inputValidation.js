565fba3e43d75e2b4639fc548b6e82c764dadab3
xiarongx <xiarongx@uci.edu>
2020-12-05 14:55:37 -0500

    fixed reg expression for username validation to allow length 3 to 50

--- inputValidation.js
+++ inputValidation.js
@@ -4,7 +4,7 @@
 1. Minimum 3 and maximum 50 characters
 2. Only uppercase letters, lowercase letters and special characters [._-] allowed
 3. Special characters are not allowed to be the first or last character, and cannot appear consecutively*/
-const usernameValidation = new RegExp("^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,50}[a-zA-Z0-9]$");
+const usernameValidation = new RegExp("^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){1,50}[a-zA-Z0-9]$");
 /*password:
 1. Minimum 8 and maximum 50 characters
 2. At least one uppercase letter, one lowercase letter, one number and one special character: [@$!%*?&].*/
